6eaad278568eb149465967cc3397353f434b369f
Andrew Le <andrewle19@csu.fullerton.edu>
2018-03-08 19:56:27 -0800

    Finished Encoding message in image, need to add options and file input/ file output, also add comments

--- textInImage.py
+++ textInImage.py
@@ -2,7 +2,7 @@ from PIL import Image
 
 def decode():
     # open and rotate the image
-    img = Image.open("testImage.png")
+    img = Image.open("Secret.png")
     img = img.rotate(180)
 
     # rgb copy of the image
@@ -25,14 +25,16 @@ def decode():
     for i in range(0,32):
         msgLengthBits.append(leastSigBits[i])
 
+
     # convert the 32 bits into an integer
+    #print(msgLengthBits)
     msgLength = int("".join(map(str,msgLengthBits)),2)
     print("The text is",msgLength,"bits long")
+    print("Or",msgLength/8,"characters long")
 
     byte = []
     msg = []
     count = 0
-
     # loop through rest of bits starting at bit right after
     # the lengths are stored
     for i in range(33, 34 + msgLength):
@@ -49,14 +51,125 @@ def decode():
         if (count == 8):
             msgIntValue = int("".join(map(str,byte)), 2)
             msg.append(chr(msgIntValue))
+            print(byte)
+
             del byte[:]
             count = 0
-
 
     msg = "".join(msg)
     print(msg)
 
     return
 
+def encode():
+    img = Image.open("TestImage.jpg")
+    img = img.rotate(180)
+    encodedImg = Image.new('RGBA',(img.width,img.height))
 
+    # make an rgba copy of the image
+    rgbImg = img.convert("RGBA")
+
+    msg = "Heisenburg I am the one who knocks"
+
+
+    # find the length of the msg
+    msgLength = len(msg)*8
+    # convert the length to binary
+    msgLength = '{0:032b}'.format(msgLength)
+    msgLength += '0'
+
+    # convert message to char list
+    msg = list(msg)
+
+    # convert msg into a binary list of characeters
+    binMsg = []
+
+    # loop through the list of characters
+    # converts each character into the byte value
+    # makes the byte value into a list of bits
+    # append the bits to the bin msg list
+    for i in range(len(msg)):
+        byteValue = '{0:08b}'.format(ord(msg[i]))
+        byteValue = list(byteValue)
+
+        for j in range(len(byteValue)):
+            binMsg.append(byteValue[j])
+
+    index = 0
+
+    binMsg = list(msgLength) + binMsg
+    print(binMsg)
+
+    # loop through entire image placing the msg
+    #in the least Sig Bit of the image
+
+    for i in range(img.height):
+        for j in range(img.width):
+            # get the rgba value of each pixel
+            (r,g,b,a) = rgbImg.getpixel((j,i))
+
+            # convert rgb into 8 bit format
+            r = '{0:08b}'.format(r)
+            g = '{0:08b}'.format(g)
+            b = '{0:08b}'.format(b)
+
+            for k in range(0,3):
+                if(index < len(binMsg)):
+
+                    if((k+1)% 3 == 1):
+                        r = r[:7]
+                        r += binMsg[index]
+                        index += 1
+
+
+                    if((k+1)%3 == 2):
+                        g = g[:7]
+                        g += binMsg[index]
+                        index += 1
+
+                    if((k+1)%3 == 0):
+                        b = b[:7]
+                        b += binMsg[index]
+                        index += 1
+
+
+
+
+
+
+            r = int(r,2)
+            g = int(g,2)
+            b = int(b,2)
+
+            encodedImg.putpixel((j,i),(r,g,b,a))
+
+    # reflip the image
+    encodedImg = encodedImg.rotate(180)
+
+    encodedImg.save('Secret.png',"PNG")
+
+
+
+
+    print(len(binMsg))
+
+    # r = '{0:08b}'.format(r)
+    # r = r[:7]
+    # # r += binMsg[0][1]
+    # print(int(r,2))
+    # for i in range(img.height):
+    #     for j in range(img.width):
+    #         if(count < len(msgLength)):
+    #             (r,g,b,a) = rgbImg.getpixel((j,i))
+    #             r = '{0:08b}'.format(r)
+    #             g = '{0:08b}'.format(g)
+    #             b = '{0:08b}'.format(b)
+
+
+
+
+
+
+
+encode()
 decode()
